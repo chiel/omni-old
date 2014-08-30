544b2b012001b87d7f5c108a08137df0703f4612
Chiel Kunkels <chiel@kunkels.me>
2014-08-31 01:41:58 +0200

    add crude saving functionality

--- modules/module/defaultrouter.js
+++ modules/module/defaultrouter.js
@@ -1,17 +1,48 @@
 'use strict';
 
-var express = require('express');
+var express = require('express'),
+	mongoose = require('mongoose'),
+	fs = require('fs'),
+	config = require('../../core/config'),
+	db = mongoose.connection;
 
 module.exports = function(modulePath, manifest){
-	var router = express.Router();
+	var router = express.Router(),
+		schema, Model;
+
+	if (fs.existsSync(modulePath + '/schema.js')){
+		schema = require(modulePath + '/schema');
+	}
+
+	if (schema && manifest.model){
+		Model = mongoose.model(manifest.model, schema);
+	}
 
 	router.get('/', function(req, res){
-		res.render(modulePath + '/views/list', {
+		var locals = {
 			unit: manifest.unit,
 			units: manifest.units,
 			listSpec: manifest.listSpec,
 			mount: manifest.mount
+		};
+
+		if (!Model){
+			console.error('No model found for module %s', manifest.name);
+			locals.error = 'No model found for this module';
+			return res.render(modulePath + '/views/list', locals);
+		}
+
+		db.on('error', console.error);
+
+		db.once('open', function() {
+			Model.find(function(err, items){
+				db.close();
+
+				locals.items = items;
+				res.render(modulePath + '/views/list', locals);
+			});
 		});
+		mongoose.connect(config.mongo);
 	});
 
 	router.get('/new/', function(req, res){
@@ -20,6 +51,28 @@ module.exports = function(modulePath, manifest){
 			units: manifest.units,
 			formSpec: manifest.formSpec
 		});
+	});
+
+	router.post('/new/', function(req, res){
+		if (!Model){
+			console.error('No model found for module %s', manifest.name);
+			return res.redirect('/' + manifest.mount + '/new/');
+		}
+
+		db.on('error', console.error);
+
+		db.once('open', function() {
+			new Model(req.body).save(function(err){
+				if (err){
+					console.log(err);
+					return res.render(modulePath + '/views/form');
+				}
+
+				db.close();
+				res.redirect('/' + manifest.mount + '/');
+			});
+		});
+		mongoose.connect(config.mongo);
 	});
 
 	return router;
---
+++ modules/role/schema.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+module.exports = new mongoose.Schema({
+	name: String,
+	modules: {}
+});
