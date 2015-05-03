af32336777da5aa73e4747ff82549340e9803ffd
Chiel Kunkels <chiel@kunkels.me>
2015-05-03 23:09:44 +0200

    core: load module adaptors

--- modules/module/module.js
+++ modules/module/module.js
@@ -2,6 +2,7 @@
 
 var fs = require('fs');
 var mongoose = require('mongoose');
+var adaptors = require('../../core/adaptors');
 var auth = require('../../core/middleware/auth');
 var generateSchema = require('./generateschema');
 var generateRouter = require('./generaterouter');
@@ -17,6 +18,7 @@ var Module = function(modulePath){
 	this.loadManifest();
 	this.loadSchema();
 	this.loadModel();
+	this.loadAdaptors();
 	this.loadApiRouter();
 	this.loadRouter();
 };
@@ -56,6 +58,20 @@ Module.prototype.loadSchema = function(){
 Module.prototype.loadModel = function(){
 	if (this.schema){
 		this.Model = mongoose.model(this.manifest.unit, this.schema, this.manifest.unit);
+	}
+};
+
+Module.prototype.loadAdaptors = function(){
+	var dir = this.path + '/adaptors';
+	if (!fs.existsSync(dir)) return;
+
+	var match;
+	var files = fs.readdirSync(dir);
+	for (var i = 0; i < files.length; i++){
+		match = files[i].match(/^(.*)\.js$/);
+		if (match){
+			adaptors[match[1]] = require(dir + '/' + match[1]);
+		}
 	}
 };
 
