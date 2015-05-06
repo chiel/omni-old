68a1afab33556dc1ec7e4026d8d92f774c82658a
Chiel Kunkels <chiel@kunkels.me>
2015-05-06 23:27:30 +0200

    update setup script to use cli arguments

--- setup.js
+++ setup.js
@@ -1,32 +1,25 @@
 #!/usr/bin/env node
 'use strict';
 
-require('./lib/loadconfig')(__dirname + '/config.json');
-require('./core/db');
+var argv = require('yargs')
+.demand([ 'mongo', 'email', 'password'])
+.argv;
 
-var readline = require('readline');
+require('mongoose').connect(argv.mongo);
+
 var Module = require('./modules/module/module');
-var mod = new Module(__dirname + '/modules/user');
+var user = new Module(__dirname + '/modules/user');
 
-var rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
+new user.Model({
+	email: argv.email,
+	password: argv.email,
+	superadmin: true
+}).save(function(err){
+	if (err){
+		console.error(err);
+		process.exit(1);
+	}
 
-rl.question('E-mail: ', function(email){
-	rl.question('Password: ', function(pass){
-		new mod.Model({
-			email: email,
-			password: pass,
-			superadmin: true
-		}).save(function(err){
-			if (err){
-				console.error(err);
-				process.exit(1);
-			}
-
-			console.log('User created');
-			process.exit(0);
-		});
-	});
+	console.log('User created');
+	process.exit(0);
 });
