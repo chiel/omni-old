5555addc3af7687b3afccee55849c4558601692e
Chiel Kunkels <chiel@kunkels.me>
2015-07-19 16:00:25 +0200

    setup: revise script, fix bug

--- setup.js
+++ setup.js
@@ -2,7 +2,7 @@
 'use strict';
 
 var argv = require('yargs')
-.demand([ 'mongo', 'email', 'password'])
+.demand([ 'mongo', 'email', 'password' ])
 .argv;
 
 require('mongoose').connect(argv.mongo);
@@ -10,16 +10,28 @@ require('mongoose').connect(argv.mongo);
 var Module = require('./modules/module/module');
 var user = new Module(__dirname + '/modules/user');
 
-new user.Model({
-	email: argv.email,
-	password: argv.email,
-	superadmin: true
-}).save(function(err){
+user.Model.findOne({ email: argv.email }, function(err, doc){
 	if (err){
 		console.error(err);
 		process.exit(1);
 	}
 
-	console.log('User created');
-	process.exit(0);
+	if (doc){
+		console.log('User already exists');
+		process.exit(0);
+	}
+
+	new user.Model({
+		email: argv.email,
+		password: argv.password,
+		superadmin: true
+	}).save(function(err){
+		if (err){
+			console.error(err);
+			process.exit(1);
+		}
+
+		console.log('User created');
+		process.exit(0);
+	});
 });
