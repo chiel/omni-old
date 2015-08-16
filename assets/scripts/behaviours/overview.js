1aaf3f5b94103a27739eb9ddaaec982796dfbd73
Chiel Kunkels <chiel@kunkels.me>
2015-08-16 14:43:47 +0200

    overview: add confirmation to delete action

--- assets/scripts/behaviours/overview.js
+++ assets/scripts/behaviours/overview.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var getParent = require('domhelpers/getParent');
+
 module.exports = function(el){
 	var checkbox = el.querySelector('thead input[type=checkbox]');
 	var checkboxes = el.querySelectorAll('tbody input[type=checkbox]');
@@ -8,6 +10,19 @@ module.exports = function(el){
 	checkbox.addEventListener('change', function(e){
 		for (i = 0; i < checkboxes.length; i++){
 			checkboxes[i].checked = checkbox.checked;
+		}
+	});
+
+	el.addEventListener('click', function(e){
+		var action = e.target.dataset.action;
+		if (!action) return;
+
+		var tr = getParent(e.target, 'tr');
+		var name = tr.querySelector(':nth-child(2)').textContent;
+		if (action === 'delete'){
+			if (!confirm('Delete item "' + name + '"?')){
+				e.preventDefault();
+			}
 		}
 	});
 };
