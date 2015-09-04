0dec861c11dadd1f03d8b58ab9a953e1f079cb34
Chiel Kunkels <chiel@kunkels.me>
2015-09-04 14:03:51 +0200

    page: update module to reflect new core

--- modules/page/manifest.js
+++ modules/page/manifest.js
@@ -1,37 +1,22 @@
 'use strict';
 
-var cache = require('../../core/cache');
-var fs = require('fs');
-var map = require('mout/array/map');
-var sort = require('mout/array/sort');
+var templates = require('../../gulp/tmp/templates');
 
 module.exports = function(manifest){
-	var modules = cache.get('modules');
-	var dir, templateDir, meta;
-	var templates = [];
-
-	modules.forEach(function(modulePath){
-		dir = modulePath + '/templates';
-		if (fs.existsSync(dir)){
-			fs.readdirSync(dir).forEach(function(templateName){
-				templateDir = dir + '/' + templateName;
-				if (fs.existsSync(templateDir + '/meta.json')){
-					meta = require(templateDir + '/meta.json');
-					meta.machine_name = templateName;
-					templates.push(meta);
-				}
-			});
-		}
-	});
-
-	manifest.formSpec.fields.template.options = sort(map(templates, function(t){
+	var options = templates.map(function(template){
 		return {
-			value: t.machine_name,
-			label: t.name || t.machine_name
+			value: template.type,
+			label: template.name
 		};
-	}), function(a, b){
+	}).sort(function(a, b){
 		return a.label > b.label ? 1 : a.label < b.label ? -1 : 0;
 	});
 
+	options.unshift({
+		value: '',
+		label: '...'
+	});
+
+	manifest.form.fields.template.options = options;
 	return manifest;
 };
