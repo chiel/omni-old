5b7d584759f5e404b6737d58ee2352a8267c44d5
Chiel Kunkels <chiel@kunkels.me>
2015-09-13 11:46:59 +0200

    builder/baseblock: submit data when done
    - accept a callback to call on submit
    - accept initial data to build form with
    - use form tag as wrapper along with submit button

--- assets/scripts/builder/blocks/base.js
+++ assets/scripts/builder/blocks/base.js
@@ -5,12 +5,14 @@ var informal = require('informal');
 /**
  * BaseBlock
  */
-var BaseBlock = function(wrap){
+var BaseBlock = function(data, wrap, callback){
 	if (!(this instanceof BaseBlock)){
-		return new BaseBlock(wrap);
+		return new BaseBlock(wrap, callback);
 	}
 
+	this.data = data;
 	this.wrap = wrap;
+	this.callback = callback;
 	this._build();
 };
 
@@ -28,23 +30,38 @@ BaseBlock.prototype.meta = BaseBlock.meta = {
 BaseBlock.prototype._build = function(){
 	if (!this.formSpec) return;
 
-	var wrap = document.createElement('div');
+	var wrap = document.createElement('form');
 	wrap.classList.add('block');
 
 	var heading = document.createElement('h2');
 	heading.textContent = this.meta.name;
 	wrap.appendChild(heading);
 
-	var form = informal(this.formSpec);
+	var form = informal(this.formSpec, this.data);
 	wrap.appendChild(form.wrap);
 
 	var btn = document.createElement('button');
 	btn.classList.add('btn', 'btn-primary', 'block-btn');
-	btn.type = 'button';
+	btn.type = 'submit';
 	btn.textContent = 'Okay';
 	wrap.appendChild(btn);
 
 	this.wrap.appendChild(wrap);
+
+	this.form = form;
+	this._setEvents();
+};
+
+/**
+ * Set block events
+ */
+BaseBlock.prototype._setEvents = function(){
+	var self = this;
+
+	self.wrap.addEventListener('submit', function(e){
+		e.preventDefault();
+		self.callback(self.form.getValues());
+	});
 };
 
 module.exports = BaseBlock;
