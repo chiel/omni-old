b8519ec4c4b5a4a00aea8d10f89055f5b28affe1
Chiel Kunkels <chiel@kunkels.me>
2015-09-19 14:27:45 +0200

    dashboard: add manifest, show title

--- modules/dashboard/router.js
+++ modules/dashboard/router.js
@@ -1,10 +1,12 @@
 'use strict';
 
-module.exports = function(){
+module.exports = function(mod){
 	var router = require('express').Router();
 
 	router.get('/', function(req, res){
-		res.render('$dashboard/index');
+		res.render('$dashboard/index', {
+			manifest: mod.manifest
+		});
 	});
 
 	return router;
