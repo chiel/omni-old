c078edac86da7a3113deff6b1dce36b8dc188794
Chiel Kunkels <chiel@kunkels.me>
2015-10-10 23:56:52 +0200

    core: expose image config to frontend

--- core/middleware/view_locals.js
+++ core/middleware/view_locals.js
@@ -1,9 +1,13 @@
 'use strict';
 
 var cache = require('../cache');
+var config = require('../config');
 var pkg = require('../../package.json');
 
 module.exports = function(req, res, next){
+	res.locals.config = {
+		images: config.images
+	};
 	res.locals.navigation = cache.get('navigation');
 	res.locals.package = pkg;
 	res.locals.user = req.user;
